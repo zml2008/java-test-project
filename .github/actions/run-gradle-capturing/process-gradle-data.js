49ee294caded1b72102d91b1f4a623579e913c7b
zml <zml@stellardrift.ca>
2021-12-25 13:13:36 -0800

    we're maybe getting javac output?

--- .github/actions/run-gradle-capturing/process-gradle-data.js
+++ .github/actions/run-gradle-capturing/process-gradle-data.js
@@ -1,6 +1,29 @@
-module.exports = async ({github, context, core, require}) => {
-    const { DATA } = process.env
-    const projectData = JSON.parse(DATA)
+const TEST_CLASSPATH = 'test-classpath.txt';
+async function processTestOutput(github, core, fs, testRoots) {
+    for (let root in testRoots) {
+        let classpath =  await fs.r
+    }
+}
 
-    console.log(`Data: ${projectData}`)
+/**
+ * Parse a javac log and produce structured [RDFormat](https://github.com/reviewdog/reviewdog/tree/master/proto/rdf) output.
+ * 
+ * This allows javac logs to be piped right into reviewdog
+ * 
+ * @param {*} javacLogs list of absolute paths to javac task outputs
+ * @param {*} fs the filesystem import
+ * @param {*} dest The path of the file to write out to
+ */
+async function javacToRdFormat(javacLogs, fs, dest) {
+
+}
+
+module.exports = async ({github, context, core, require}) => {
+    const { DATA } = process.env;
+    const projectData = JSON.parse(DATA);
+    const fs = require('fs/promises');
+
+    let testPromise = processTestOutput(github, core, fs, projectData.testExecutions);
+
+    console.log(`Data: ${projectData.keys()}`)
 }
